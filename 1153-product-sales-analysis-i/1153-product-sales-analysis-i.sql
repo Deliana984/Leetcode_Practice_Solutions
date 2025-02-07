aebd6ab7b957d577a45e92a3341fd6e3092d46dc
Deliana W <118982842+Deliana984@users.noreply.github.com>
2025-02-08 06:47:47 +0700

    Time: 2103 ms (57.87%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-select Product.product_name, Sales.year, Sales.price from Sales RIGHT JOIN Product 
+select Product.product_name, Sales.year, Sales.price from Sales JOIN Product 
 ON Sales.product_id = Product.product_id where Sales.year IS NOT NULL AND Sales.price IS NOT NULL;
